87603fdc413043de0fe8b431fcdc8450c0c9fe76
1mAgiN4ry <125927980+1mAgiN4ry@users.noreply.github.com>
2025-05-14 20:54:38 +0100

    pondered

--- scripts/form_confirmation.js
+++ scripts/form_confirmation.js
@@ -1,11 +1,15 @@
 export function confirmation(form) {
     const screen = document.getElementById("confirmation-screen");
+    const text = document.getElementById("pop-up-text");
     const confirm = document.getElementById("confirm");
     const cancel = document.getElementById("cancel");
 
     let listenersAttached = false;
     
     screen.style.display = "flex";
+    text.style.display = "inline-block";
+    confirm.style.display = "inline-block";
+    cancel.style.display = "inline-block";
 
     if (!listenersAttached) {
         confirm.addEventListener("click", function () {
@@ -14,6 +18,9 @@ export function confirmation(form) {
 
         cancel.addEventListener("click", function () {
             screen.style.display = "none";
+            text.style.display = "none";
+            confirm.style.display = "none";
+            cancel.style.display = "none";
         });
 
         listenersAttached = true;
